1e9a28844b54a309c7d68ec541a4a8a0c58efba5
Ovramets <oleg.ovramets@teamvoy.com>
2023-11-13 00:21:21 +0200

    create react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  // <React.StrictMode> // Maybe add later
+    <App />
+  // </React.StrictMode>
+);
